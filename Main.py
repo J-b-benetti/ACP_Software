bab8a558092ac34b3962a9e99296eed45e6873b3
J-b-benetti <j.b.benetti81@gmail.com>
2022-10-27 21:09:57 +0200

    Début d'ajout du calcul du cercle de corrélation

--- Main.py
+++ Main.py
@@ -14,36 +14,46 @@ import matplotlib.pyplot as plt
 ##              [6, 15],
 ##              [8, 10]])
 
-numRows = int(input("Combien de lignes?") ) #on demande à l'utilisateur de rentrer le nombre de ligne de la matrice
-numColumns = int(input("Combien de colonnes?")) #on demande à l'utilisateur de rentrer le nombre de colonne de la matrice
+X = np.array([[-4, -5],
+              [0, 11],
+              [3, 10],
+              [6, 12],
+              [8, -4],
+              [5, 0]])
+
+##numRows = int(input("Combien de lignes?") ) #on demande à l'utilisateur de rentrer le nombre de ligne de la matrice
+##numColumns = int(input("Combien de colonnes?")) #on demande à l'utilisateur de rentrer le nombre de colonne de la matrice
 
 #tableau dynamique
-X = []
-for i in range(numRows):
-    X.append([0] * numColumns)
-    for j in range(numColumns):
-        X[i][j] = float(input("Entrer les valeurs de la matrice"))
-        print(X)
-X = np.array(X)
+##X = []
+##for i in range(numRows):
+##    X.append([0] * numColumns)
+##    for j in range(numColumns):
+##        X[i][j] = float(input("Entrer les valeurs de la matrice"))
+##        print(X)
+##X = np.array(X)
 print("La matrice de départ X vaut: \n", X, "\n")
 
 X_std = StandardScaler().fit_transform(X)
-print("Standardisation des caractéristiques en supprimant la moyenne et en mettant à l'échelle la variance unitaire: \n", X_std, "\n")
+print("Standardisation des caractéristiques en supprimant la moyenne et en mettant à l'échelle la variance unitaire. En d'autres termes, la matrice 'Z' vaut: \n", X_std, "\n")
 
 features = X_std.T
 correlation_matrix = np.cov(features)
 print("La matrice de corrélation (après mise à l'echelle) vaut: \n", correlation_matrix, "\n")
 
+corr_matrix_without_scale = np.corrcoef(X.T)
+vals, vects = eig(corr_matrix_without_scale)
 
-eigVects_after_scaled, eigVals_after_scaled = eig(correlation_matrix)
-print("Les valeurs propres de la matrice de corrélation (après mise à l'echelle) sont: \n", eigVects_after_scaled, "\n")
-print("Les vecteurs propres de la matrice de corrélation (après mise à l'echelle) sont: \n", eigVals_after_scaled, "\n")
+eigVals_after_scaled, eigVects_after_scaled = eig(correlation_matrix)
+print("Les valeurs propres de la matrice de corrélation (après mise à l'echelle) sont: \n", eigVals_after_scaled, "\n")
+print("Les vecteurs propres de la matrice de corrélation (après mise à l'echelle) sont: \n", eigVects_after_scaled, "\n")
 
-pca = PCA(n_components = 2)
+n_components = 2
+pca = PCA(n_components = n_components)
 principal_components = pca.fit_transform(X_std)
 new_X = pd.DataFrame(data = principal_components, columns = ['Composante principale 1', 'Principale composante 2'])
 new_X.head()    #matrice 'Psi' sous forme de tableau grâce à Pandas
-print("La matrice 'Psi' sous forme de tableau vaut: \n", new_X)
+print("La matrice 'Psi' sous forme de tableau vaut: \n", new_X.head())
 
 pourcentage_variance = np.round(pca.explained_variance_ratio_*100, decimals = 1)
 label = ['PC' + str(x) for x in range(1, len(pourcentage_variance) + 1)]
@@ -54,10 +64,10 @@ plt.title("Graphe de pourcentage des composantes principales")
 #plt.show()
 
 color = 'red'
-#n = ["ind1", "ind2", "ind3", "ind4"]   #tableau rentré de manière statique
-n = []  #tableau rentré de manière dynamique
-for i in range(numRows):
-    n.append("Ind" + str(i + 1))
+n = ["ind1", "ind2", "ind3", "ind4"]   #tableau rentré de manière statique
+##n = []  #tableau rentré de manière dynamique
+##for i in range(numRows):
+##    n.append("Ind" + str(i + 1))
 
 plt.figure(figsize = (2, 2))    #dimensionnement de la fenêtre
 plt.scatter(principal_components[:, 0], principal_components[:, 1], c = color, cmap = 'viridis', alpha = 1)
@@ -67,5 +77,37 @@ for i, txt in enumerate(n):
 plt.xlabel("Composante principale 1")
 plt.ylabel("Composante principale 2")
 plt.title("Plan principal 'psi1' et 'psi2'")
-plt.show()  #on affiche les graphes
+#plt.show()  #on affiche les graphes
+
+print(vals[0])
+print(vals[1])
+print(eigVects_after_scaled[0])
+
+
+
+
+##theta = np.linspace(0, 2*np.pi, 100)
+##
+##r = np.sqrt(1.0)
+##
+##x1 = r*np.cos(theta)
+##x2 = r*np.sin(theta)
+##x3 = 0.5
+##x4 = 0.7
+##
+##fig, ax = plt.subplots(1)
+##
+##ax.plot(x1, x2)
+##plt.plot(x3, x4, marker="o", color="red")
+##ax.set_aspect(1)
+##
+##plt.xlim(-1.25,1.25)
+##plt.ylim(-1.25,1.25)
+##
+##plt.grid(linestyle='--')
+##
+##plt.title('How to plot a circle with matplotlib ?', fontsize=8)
+##
+##
+##plt.show()
 
